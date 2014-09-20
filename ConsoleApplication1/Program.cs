54b6012748bee5dadd1621d0ac25964a5dad197b
lsk <lkorokh@gmail.com>
2014-09-21 00:23:48 +0400

    ds

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine(@"Here we go!!");
+            System.Console.WriteLine(@"Here we go!! ");
         }
     }
 }
